c762f7b9f4654b5452863311153a641b024316bf
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-05 04:18:23 +0000

    some package cleanup for function rewriting
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reol@3 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/DownloadEOLpages.R
+++ pkg/R/DownloadEOLpages.R
@@ -1,9 +1,13 @@
 DownloadEOLpages <- function(pages, MyKey) {
 #Download xml page content  #now full content using X=75
-	for (i in sequence(length(pages))){
-		pageNum<-pages[i]
-		print(i)
-		system(paste("curl 'http://eol.org/api/pages/", pageNum, ".xml?key=", MyKey, "&images=75&videos=75&sounds=75&maps=75&text=75&iucn=1'", " -o eol", pageNum,".xml -a", sep=""))
-		Sys.sleep(1)
+  for (i in sequence(length(pages))){
+    pageNum<-pages[i]
+    print(i)
+    #system(paste("curl 'http://eol.org/api/pages/", pageNum, ".xml?key=", MyKey, "&images=75&videos=75&sounds=75&maps=75&text=75&iucn=1'", " -o eol", pageNum,".xml -a", sep=""))
+	web <- paste("http://eol.org/api/pages/", pageNum, ".xml?key=", MyKey, "&images=75&videos=75&sounds=75&maps=75&text=75&subjects=all&details=true&common_names=true&references=true", sep="")	
+    a <- getURL(web)
+    #save these into files somehow
+
+    Sys.sleep(1)
 	}
 }
