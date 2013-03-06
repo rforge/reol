bc65ad23e78bf6896c4dd0dae357bfb6d860e978
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-06 17:30:30 +0000

    updated DownloadEOLpages and added man
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reol@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/DownloadEOLpages.R
+++ pkg/R/DownloadEOLpages.R
@@ -1,13 +1,12 @@
-DownloadEOLpages <- function(pages, MyKey) {
+DownloadEOLpages <- function(pages, MyKey=NULL) {
 #Download xml page content  #now full content using X=75
   for (i in sequence(length(pages))){
     pageNum<-pages[i]
-    print(i)
-    #system(paste("curl 'http://eol.org/api/pages/", pageNum, ".xml?key=", MyKey, "&images=75&videos=75&sounds=75&maps=75&text=75&iucn=1'", " -o eol", pageNum,".xml -a", sep=""))
-	web <- paste("http://eol.org/api/pages/", pageNum, ".xml?key=", MyKey, "&images=75&videos=75&sounds=75&maps=75&text=75&subjects=all&details=true&common_names=true&references=true", sep="")	
+    web <- paste("http://eol.org/api/pages/", pageNum, ".xml?images=75&videos=75&sounds=75&maps=75&text=75&subjects=all&details=true&common_names=true&references=true", sep="")	
+	if(!is.null(MyKey))
+      web <- paste(web, "&key=", MyKey, sep="")
     a <- getURL(web)
-    #save these into files somehow
-
+    saveXML(xmlRoot(xmlTreeParse(a)), file=paste("eol", pages[i], ".xml", sep=""))
     Sys.sleep(1)
-	}
+  }
 }
