0aa95d344ac6c144c0687a21daa8803416c59c39
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-03-25 03:19:37 +0000

    changed GetReferences
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reol@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/GetReferences.R
+++ pkg/R/GetReferences.R
@@ -1,26 +1,24 @@
 GetReferences <- function(MyFiles, output=c("detail", "counts")) {
   #This returns a data frame with all common names as a separate row.  Maybe make a data frame with an overview of information (number of cns in english, etc. for each sp.)
   output <- match.arg(output)
-  References <- c()
-  Taxon <- c()
-  RefCounts <- c()
+  ReferenceList <- matrix(nrow=0, ncol=3)
+  colnames(ReferenceList) <- c("Taxon", "eolID", "Reference")
+  RefCounts <- matrix(nrow=length(MyFiles), ncol=3)
+  colnames(RefCounts) <- c("Taxon", "eolID", "Number Of References")
   for(i in sequence(length(MyFiles))) {
     res <- xmlToList(xmlRoot(xmlParse(MyFiles[i], getDTD=FALSE)), simplify=FALSE)$taxonConcept
     whichReferences <- which(names(res) == "reference")
-    RefCounts <- rbind(RefCounts, c(res$ScientificName, res$taxonConceptID, length(whichReferences)))
-    RefCounts <- data.frame(RefCounts, stringsAsFactors=F)
-    colnames(RefCounts) <- c("Taxon", "eolID", "Number Of References")
+    RefCounts[i,] <- c(res$ScientificName, res$taxonConceptID, length(whichReferences))
     for(j in 1:length(whichReferences)) {
-      #if(!is.null(res[whichReferences[j]])){  #commented out because some taxa do not have references, so they come in NULL.  
-        Taxon <- append(Taxon, res$taxonConceptID)
-        References <- append(References, res[whichReferences[j]])
+      #if(!is.null(res[whichReferences[j]])){  #commented out NULL, because come in "NULL" so is.null doesn't work
+        ReferenceList <- rbind(ReferenceList , c(res$ScientificName, res$taxonConceptID, as.character (res[whichReferences[j]])))
       #}
     }
   }
-  if(output == "detail") {
-    ReferenceList <- cbind(Taxon, References)
+  RefCounts <- data.frame(RefCounts, stringsAsFactors=F)
+  ReferenceList <- data.frame(ReferenceList, stringsAsFactors=F)
+  if(output == "detail")
     return(ReferenceList)
-  }
   if(output == "counts")
     return(RefCounts)
 }
