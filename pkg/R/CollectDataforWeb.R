733152058d15ec7d91edbdc4b141f74f7774c26d
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-24 21:13:57 +0000

    package failing to build on linux, checking to see if it is an issue with R.oo
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reol@72 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/CollectDataforWeb.R
+++ pkg/R/CollectDataforWeb.R
@@ -1,3 +1,12 @@
+PageProcessing <- function(MyEOL) {
+  res <- xmlToList(xmlRoot(xmlParse(MyEOL, getDTD=FALSE)), simplify=FALSE)
+  if(!is.null(res$error)) {
+    system(paste("mv", MyEOL, "../TRASH/"))
+    stop(paste("Bad file", MyEOL, "has been purged"))
+    }
+  return(res)
+}
+
 FirstTwo <- function(name) {
   if(is.null(name))
     name <- NA
