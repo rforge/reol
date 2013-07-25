b8da145f2a4cec7c8f45a2cb3dffce654cef1805
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-25 19:40:15 +0000

    updated IUCN functions and man files
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reol@77 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/GetIUCNStat.R
+++ pkg/R/GetIUCNStat.R
@@ -1,19 +1,21 @@
-#IUCN
-GetIUCNStat <- function(MyEOLs) {
+GetIUCNStat <- function(MyEOLs, from.file=TRUE) {
   IUCN <- matrix(ncol=3, nrow=length(MyEOLs))
   colnames(IUCN) <- c("Taxon", "eolID", "IUCNstat")
   for(i in sequence(length(MyEOLs))){
-    res <- PageProcessing(MyEOLs[i])
+    if(from.file)
+      res <- PageProcessing(MyEOLs[i])
+    else
+      res <- PageProcessing(MyEOLs[[i]])
     whichDOs <- which(names(res) == "dataObject")
     IUCNstat <- NA
-    for(j in sequence(length(whichDOs))) {
+    for(j in sequence(length(whichDOs))) {  #cleaner/faster way to do this would be to grep for IUCN throughout all of res, get location and use that
       if(!is.null(res[[whichDOs[j]]]$title)) {
         if(res[[whichDOs[j]]]$title == "IUCNConservationStatus")
           IUCNstat <- res[[whichDOs[j]]]$description
       }
     }
-  
-  IUCN[i,] <- c(res$taxonConcept$ScientificName, res$taxonConcept$taxonConceptID, IUCNstat)
+  scientificName  <- res$taxonConcept[[which(names(res$taxonConcept) == grep("ScientificName", names(res$taxonConcept), ignore.case=TRUE, value=T))]] #because some are cap and some are not
+  IUCN[i,] <- c(scientificName, res$taxonConcept$taxonConceptID, IUCNstat)
   }  
   return(IUCN)
 }
