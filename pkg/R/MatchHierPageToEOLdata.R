b4f017ab279fe629c2cd2761820526a0eff9d298
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-31 22:49:37 +0000

    made match eol data to Hier numbers for trees function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reol@89 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/MatchHierPageToEOLdata.R
+++ pkg/R/MatchHierPageToEOLdata.R
@@ -1,13 +1,18 @@
 MatchHierPageToEOLdata <- function(MyHiers, EOLdata){
-  if(any(MyHiers == "hierNA.xml"))
-  	MyHiers <- MyHiers[-which(MyHiers == "hierNA.xml")]
+  if(any(is.na(names(MyHiers)))) {
+  	whichNAs <- which(is.na(names(MyHiers)))
+  	MyHiers <- MyHiers[-whichNAs]
+  }
+  if(class(MyHiers) == "list")
+    fileNames <- names(MyHiers)
+  else
+    fileNames <- MyHiers
   matchedData <- matrix(nrow=length(MyHiers), ncol=3)
-  #colnames(matchedData) <- c( "HierTaxon", "HierID", "eolID")
-  matchedData[,2] <- as.character(sapply(MyHiers, GetHierID))
+  matchedData[,2] <- as.character(sapply(fileNames, GetHierID))
   for(i in sequence(length(MyHiers))) {
     resOneFile<-OneFileHierarchy(MyHiers[i])
-    matchedData[i,1] <- resOneFile[which(matchedData[i,2]==resOneFile[,6]), 1]
-    matchedData[i,3] <- resOneFile[which(matchedData[i,2]==resOneFile[,6]), 4]
+    matchedData[i,1] <- resOneFile[which(matchedData[i,2]==resOneFile[,6]), 1] #taxonName
+    matchedData[i,3] <- resOneFile[which(matchedData[i,2]==resOneFile[,6]), 4] #HierID
   }
   matches <- match(matchedData[,3], EOLdata[,2])
   matchedData <- cbind(matchedData, EOLdata[matches, 3:dim(EOLdata)[2]])
