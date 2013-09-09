efa860660634d26a72b15b94ebddb22a93b0f3aa
bbanbury <bbanbury@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-09-09 21:53:40 +0000

    updated some more man files and the GetHierID function
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/reol@102 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/GetHierID.R
+++ pkg/R/GetHierID.R
@@ -1,5 +1,7 @@
 GetHierID <- function(MyHier){
-  conceptID <- gsub("hier", "", MyHier)
-  conceptID <- gsub(".xml", "", conceptID)
+  if(class(MyHier) == "list")
+    conceptID <- gsub("^\\D+|\\D+$", "", names(MyHier))
+  if(class(MyHier) == "character")
+    conceptID <- gsub("^\\D+|\\D+$", "", MyHier)
   return(conceptID)
 }
